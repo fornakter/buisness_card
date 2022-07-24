4c7e34a6104829eec202902f0d364f987a57d556
Adam <fornakter@gmail.com>
2022-07-24 19:18:55 +0200

    First files ready

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('/main.html')
+
+
+@app.route('/kontakt')
+def kontakt():
+    return render_template('/kontakt.html')
+
+
+@app.route('/o_nas')
+def about():
+    return render_template('/o_nas.html')
+
+
+@app.route('/projects')
+def projects():
+    return render_template('/projects.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
