79f6e30e1202261057976cf151d0a133ccd29c4e
Sławomir Jałojda <slawekjalojda@gmail.com>
2022-07-26 19:13:13 +0200

    Add form in kontakt.html, add module send mail

--- main.py
+++ main.py
@@ -5,22 +5,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('/main.html')
+    return render_template('main.html')
 
 
-@app.route('/kontakt')
+@app.route('/kontakt/')
 def kontakt():
-    return render_template('/kontakt.html')
+    return render_template('kontakt.html')
 
 
-@app.route('/o_nas')
+@app.route('/o_nas/')
 def about():
-    return render_template('/o_nas.html')
+    return render_template('o_nas.html')
 
 
-@app.route('/projects')
+@app.route('/projects/')
 def projects():
-    return render_template('/projects.html')
+    return render_template('projects.html')
 
 
 
