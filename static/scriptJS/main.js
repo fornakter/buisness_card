95103dd37e2f3d284347d910fa55c2f78069dab0
Łukasz Sadowski <Lukasz.sadowski2605@gmail.com>
2022-09-08 09:08:38 +0200

    added rotataing footer

--- static/scriptJS/main.js
+++ static/scriptJS/main.js
@@ -1,3 +1,17 @@
 import { changeNavbar } from "./navbar.js";
 
 changeNavbar();
+
+document.addEventListener("DOMContentLoaded", () => {
+  const nav = document.querySelector(".navbar");
+
+  const addShadow = () => {
+    if (window.scrollY >= 30) {
+      nav.classList.add("bg-shadow");
+    } else {
+      nav.classList.remove("bg-shadow");
+    }
+  };
+
+  window.addEventListener("scroll", addShadow);
+});
