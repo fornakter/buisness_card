ff52fd57cec0cbd993b1dc027c6a1c3746f5732d
KrzysiekLu <krzysztof.luuczak@gmail.com>
2022-09-18 17:24:59 +0200

    fix

--- static/scriptJS/navbarObserver.js
+++ static/scriptJS/navbarObserver.js
@@ -1,19 +1,27 @@
 export const navbarObserver = () => {
   const navbar = document.querySelector(".navbar");
+  const logo = document.querySelector(".logo");
 
   const navObserverOptions = {
     rootMargin: "200px 0px -200px 0px ",
     threshold: 1.0,
   };
-  const navObserver = new IntersectionObserver((e) => {
+  //   const navObserver = new IntersectionObserver((e) => {
+  //     !e[0].isIntersecting
+  //       ? (navbar.classList.add("navbar--small"),
+  //         navbar.classList.remove("navbar"))
+  //       :
+  //       ;
+  //   }, navObserverOptions);
+
+  //   navObserver.observe(navbar);
+
+  const logoObserver = new IntersectionObserver((e) => {
     console.log(e);
-    console.log(navbar);
 
     !e[0].isIntersecting
-      ? (navbar.classList.remove("navbar"),
-        navbar.classList.add("navbar--small"))
-      : (navbar.classList.add("navbar"),
-        navbar.classList.remove("navbar--small"));
+      ? navbar.classList.add("navbar--small")
+      : navbar.classList.remove("navbar--small");
   }, navObserverOptions);
-  navObserver.observe(navbar);
+  logoObserver.observe(logo);
 };
