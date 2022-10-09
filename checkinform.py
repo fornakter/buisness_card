0c8cb0750c656d8409ad92d451dc385c3401b173
AdamF <fornakter@gmail.com>
2022-10-09 09:35:47 +0200

    PEP8

--- checkinform.py
+++ checkinform.py
@@ -1,22 +1,25 @@
 # sprawdzenie poprawności wpisanego maila
 import re
 
+
 def checkMail(mail):
-    #sprawdzenie czy wpisane dane w formularzu to adres mailowy, sprawdzenie czy znajduje się 1 znak @ i 1 znak .
+    # sprawdzenie czy wpisane dane w formularzu to adres mailowy, sprawdzenie czy znajduje się 1 znak @ i 1 znak .
     pat = "^[a-zA-Z0-9-_]+@[a-zA-Z0-9]+\.[a-z]{1,3}$"
     if re.match(pat, mail):
         return True
     else:
         return False
 
-#sprawdzenie wiadomości
+
+# sprawdzenie wiadomości
 def checkMessage(message):
     if len(message) > 0:
         return True
     else:
         return False
 
-#sprawdzenie imienia
+
+# sprawdzenie imienia
 def checkName(name):
     if len(name) > 0:
         return True
